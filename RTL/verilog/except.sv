c7a40b8f2275ac0bb6aab01ef38d008cd0ce0986
Gene <84300095+xbicai@users.noreply.github.com>
2024-12-05 22:12:51 -0800

    Update except.sv

--- RTL/verilog/except.sv
+++ RTL/verilog/except.sv
@@ -1,73 +1,103 @@
+/////////////////////////////////////////////////////////////////////
+////                                                             ////
+////  EXCEPT                                                     ////
+////  Floating Point Exception/Special Numbers Unit              ////
+////  ECE 571 Group 17 V->SV conversion
+//    Original by 
+////  Author: Rudolf Usselmann                                   ////
+////          rudi@asics.ws                                      ////
+
+
+
+
 `timescale 1ns / 100ps
 
-module except (
-input	logic clk,
-input	logic [31:0]opa, opb,
-output	logic inf, 
-output	logic ind, 
-output	logic qnan, 
-output	logic snan, 
-output	logic opa_nan, 
-output	logic opb_nan,
-output	logic opa_00, 
-output	logic opb_00,
-output  logic opa_inf, 
-output	logic opb_inf,
-output  logic opa_dn,
-output	logic opb_dn
+
+module except(
+input logic		clk,
+input logic [31:0]	opa, opb,
+output logic	inf, ind, qnan, snan, opa_nan, opb_nan,
+output logic	opa_00, opb_00,
+output logic	opa_inf, opb_inf,
+output logic	opa_dn,
+output logic	opb_dn
 );
 
-//local wires and registers
+
+////////////////////////////////////////////////////////////////////////
+//
+// Local Wires and registers
+//
+
 logic	[7:0]	expa, expb;		// alias to opX exponent
 logic	[22:0]	fracta, fractb;		// alias to opX fraction
-logic	expa_ff, infa_f_r, qnan_r_a, snan_r_a;
-logic	expb_ff, infb_f_r, qnan_r_b, snan_r_b;
-logic   expa_00, expb_00, fracta_00, fractb_00;
+logic		expa_ff, infa_f_r, qnan_r_a, snan_r_a;
+logic		expb_ff, infb_f_r, qnan_r_b, snan_r_b;
+logic		expa_00, expb_00, fracta_00, fractb_00;
 
 
+////////////////////////////////////////////////////////////////////////
+//
 // Aliases
+//
+
 assign   expa = opa[30:23];
 assign   expb = opb[30:23];
 assign fracta = opa[22:0];
 assign fractb = opb[22:0];
 
-
-
+////////////////////////////////////////////////////////////////////////
+//
+// Determine if any of the input operators is a INF or NAN or any other special number
+//
 
 always_ff @(posedge clk) begin
-    expa_ff <= #1 &expa;
-    expb_ff <= #1 &expb;
+	expa_ff <= #1 &expa;
 
-    infa_f_r <= #1 !(|fracta);
-    infb_f_r <= #1 !(|fractb);
+	expb_ff <= #1 &expb;
 
-    qnan_r_a <= #1 fracta[22];
-    snan_r_a <= #1 fracta[22] & |fracta[21:0];
-    qnan_r_b <= #1 fractb[22];
-    snan_r_b <= #1 !fractb[22] & |fractb[21:0];
+	infa_f_r <= #1 !(|fracta);
 
-    ind  <= #1 (expa_ff & infa_f_r) & (expb_ff & infb_f_r);
-    inf  <= #1 (expa_ff & infa_f_r) | (expb_ff & infb_f_r);
+	infb_f_r <= #1 !(|fractb);
 
-    qnan <= #1 (expa_ff & qnan_r_a) | (expb_ff & qnan_r_b);
-    snan <= #1 (expa_ff & snan_r_a) | (expb_ff & snan_r_b);
+	qnan_r_a <= #1  fracta[22];
 
-    opa_nan <= #1 &expa & (|fracta[22:0]);
-    opb_nan <= #1 &expb & (|fractb[22:0]);
+	snan_r_a <= #1 !fracta[22] & |fracta[21:0];
 
-    opa_inf <= #1 (expa_ff & infa_f_r);
-    opb_inf <= #1 (expb_ff & infb_f_r);
+	qnan_r_b <= #1  fractb[22];
 
-    expa_00 <= #1 !(|expa);
-    expb_00 <= #1 !(|expb);
+	snan_r_b <= #1 !fractb[22] & |fractb[21:0];
 
-    fracta_00 <= #1 !(|fracta);
-    fractb_00 <= #1 !(|fractb);
+	ind  <= #1 (expa_ff & infa_f_r) & (expb_ff & infb_f_r);
 
-    opa_00 <= #1 expa_00 & fracta_00;
-    opb_00 <= #1 expb_00 & fractb_00;
-    opa_dn <= #1 expa_00;
-    opb_dn <= #1 expb_00;
+	inf  <= #1 (expa_ff & infa_f_r) | (expb_ff & infb_f_r);
+
+	qnan <= #1 (expa_ff & qnan_r_a) | (expb_ff & qnan_r_b);
+
+	snan <= #1 (expa_ff & snan_r_a) | (expb_ff & snan_r_b);
+
+	opa_nan <= #1 &expa & (|fracta[22:0]);
+
+	opb_nan <= #1 &expb & (|fractb[22:0]);
+
+	opa_inf <= #1 (expa_ff & infa_f_r);
+
+	opb_inf <= #1 (expb_ff & infb_f_r);
+
+	expa_00 <= #1 !(|expa);
+
+	expb_00 <= #1 !(|expb);
+
+	fracta_00 <= #1 !(|fracta);
+
+	fractb_00 <= #1 !(|fractb);
+
+	opa_00 <= #1 expa_00 & fracta_00;
+
+	opb_00 <= #1 expb_00 & fractb_00;
+
+	opa_dn <= #1 expa_00;
+
+	opb_dn <= #1 expb_00;
 end
-
 endmodule
